38382e43f0ce5d7e52e020348da29659db5e7ae9
Sebastian Ardila Trujillo <ardilatruji@gmail.com>
2022-11-22 22:14:53 -0500

    Class 22-11

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const conectarDB = async () => {
     try{
         const connection = await mongoose.connect(
+            //"mongodb+srv://AsCrack:Tato@cluster0.10a2vet.mongodb.net/?retryWrites=true&w=majority", {
            "mongodb+srv://sardilat:killershield94@cluster0.viswkol.mongodb.net/?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,7 +1,20 @@
 const Usuario = require("../models/usuario.js");
+const bcryptjs = require("bcryptjs")
 
 exports.crearUsuario = async (req, res) => {
-    console.log(req.body);
-    res.json({msg: "desde controller post"})
+    //console.log(req.body);
+    const { password } = req.body
+
+    try{
+        //crear un nuevo usuario
+        let usuario = new Usuario(req.body);
+        //hash
+        usuario.password = await bcryptjs.hash(password, 10); //10 es la cantidad de rondas que hace para encriptar el password
+        //Guardar usuario en la bd
+        const usuarioAlmacenado = await usuario.save();
+        res.json(usuarioAlmacenado);
+    }catch(error){
+        console.log(error);
+    }
 };
 
